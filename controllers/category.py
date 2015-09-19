38839cc0c75ef2b5a6e86100b33238c30b398e68
Daniel J. Ramirez <danielramirezz123@gmail.com>
2015-09-19 13:00:34 -0500

    automatic category url name generation and sanitization

--- controllers/category.py
+++ controllers/category.py
@@ -2,6 +2,28 @@
 #
 # Author: Daniel J. Ramirez
 
+import urllib
+import re
+
+
+rmap = {  'á': 'a', 'Á': 'a' , 'é': 'e', 'É': 'e' , 'í': 'i', 'Í': 'i'
+        , 'ó': 'o', 'Ó': 'o' , 'ú': 'u', 'Ú': 'u'
+       }
+def replace_match(match):
+    key = match.group(0)
+    print key
+    if rmap.has_key(key):
+        return rmap[key]
+    return ""
+regex = re.compile('(á|é|í|ó|ú|Á|É|Í|Ó|Ú)')
+
+def urlify_string(string):
+    s = regex.sub(replace_match, string)
+    s = urllib.quote(s)
+    return s
+
+
+
 def create():
     redirect_url = URL('index')
     parent = db.category(request.vars.parent)
@@ -14,6 +36,8 @@ def create():
         form.vars.parent = parent.id
 
     if form.process().accepted:
+        url_name = "%s%s" % (urlify_string(form.vars.name), form.vars.id)
+        db.category(form.vars.id).update_record(url_name=url_name)
         response.flash = T("category created")
         redirect(redirect_url)
     elif form.errors:
@@ -44,6 +68,8 @@ def update():
         form = SQLFORM(db.category, category, fields=['name', 'description', 'url_name', 'icon', 'trait_category1', 'trait_category2', 'trait_category3'])
 
     if form.process().accepted:
+        url_name = "%s%s" % (urlify_string(form.vars.name), form.vars.id)
+        db.category(form.vars.id).update_record(url_name=url_name)
         response.flash = T("category created")
         redirect(redirect_url)
     elif form.errors:
