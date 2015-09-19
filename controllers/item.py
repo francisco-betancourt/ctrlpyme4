e726a5af26bf1f2302cedfcb6c879d0c7ec3096e
Daniel J. Ramirez <danielramirezz123@gmail.com>
2015-09-19 13:17:27 -0500

    SQLFORM in item creation, fixed issue with single category grouping in the category tree

--- controllers/item.py
+++ controllers/item.py
@@ -5,9 +5,46 @@
 import json
 
 
+def categories_tree(categories):
+    current_category = categories.first().parent
+    categories_children = {}
+    current_tree = []
+    for category in categories:
+        if category.parent != current_category:
+            categories_children[current_category] = current_tree
+            current_tree = []
+            current_category = category.parent
+        # current_tree.append({'text': category.name})
+        child = {'text': category.name, 'category_id': category.id}
+        if categories_children.has_key(category.id):
+            child['nodes'] = categories_children[category.id]
+            current_tree.append(child)
+            if category.parent:
+                del categories_children[category.id]
+        else:
+            current_tree.append(child)
+    categories_children[current_category] = current_tree
+    current_tree = []
+    # the categories_children array contains the master categories.
+    categories_tree = []
+    for subtree in categories_children.itervalues():
+        categories_tree.append(subtree)
+    # json object from python dict
+    categories_tree = json.dumps(categories_tree[0])
+    category_search = INPUT(_type="search", _id="category_search", _placeholder=T("search categories"), _class="form-control")
+    categories_selected = INPUT(_type="text", _id="categories_selected", _hidden=True, _name="categories_selected")
+    # hack: this script sets the javascript variable categories_tree_data, to
+    # the categories_tree json data, so it can be used inside the view.
+    categories_tree_script = SCRIPT("categories_tree_data = %s;" % categories_tree)
+    field = DIV(category_search, DIV(_id="categories_tree"), categories_selected, categories_tree_script, _class="col-sm-9")
+
+    return DIV(LABEL(T('Categories'), _class="control-label col-sm-3"), field, _class="form-group")
+
+
+
 def create():
 
-    form = SQLFORM(db.category, fields=['name', 'description'])
+    form = SQLFORM(db.item, fields=['name', 'description', 'is_bundle'])
 
     # brand
     field = SELECT(OPTION(""), _name='id_brand', _class="form-control")
@@ -21,40 +58,10 @@ def create():
     categories = db((db.category.id > 0) &
                     (db.category.is_active==True)
                    ).select(orderby=~db.category.parent)
-    current_category = categories.first().parent
-    current_tree = []
-    categories_children = {}
-    for category in categories:
-        if category.parent != current_category:
-            # current_tree = [{"text": current_category, 'nodes': current_tree}]
-            categories_children[current_category] = current_tree
-            current_tree = []
-            current_category = category.parent
-        # current_tree.append({'text': category.name})
-        child = {'text': category.name, 'category_id': category.id}
-        if categories_children.has_key(category.id):
-            child['nodes'] = categories_children[category.id]
-            current_tree.append(child)
-            if category.parent:
-                del categories_children[category.id]
-        else:
-            current_tree.append(child)
-    # the categories_children array contains the master categories.
-    categories_tree = []
-    for subtree in categories_children.itervalues():
-        categories_tree.append(subtree)
-    # json object from python dict
-    categories_tree = json.dumps(categories_tree[0])
-    category_search = INPUT(_type="search", _id="category_search", _placeholder=T("search categories"), _class="form-control")
-    categories_selected = INPUT(_type="text", _id="categories_selected", _hidden=True, _name="categories_selected")
-    # hack: this script sets the javascript variable categories_tree_data, to
-    # the categories_tree json data, so it can be used inside the view.
-    categories_tree_script = SCRIPT("categories_tree_data = %s;" % categories_tree)
-    field = DIV(category_search, DIV(_id="categories_tree"), categories_selected, categories_tree_script, _class="col-sm-9")
-    form[0].insert(1, DIV(LABEL(T('Categories'), _class="control-label col-sm-3"), field, _class="form-group"))
+    if categories:
+        form[0].insert(1, categories_tree(categories))
 
     if form.process().accepted:
-        print form.vars
         response.flash = 'form accepted'
     elif form.errors:
         response.flash = 'form has errors'
