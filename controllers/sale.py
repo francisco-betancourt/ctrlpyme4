73052018adebc3e6701590de3d6d16f4c0ecaa47
Daniel J. Ramirez <danielramirezz123@gmail.com>
2015-10-22 14:39:36 -0500

    Basic payments functionallity for sales

--- controllers/sale.py
+++ controllers/sale.py
@@ -2,6 +2,8 @@
 #
 # Author: Daniel J. Ramirez
 
+import json
+
 
 @auth.requires(auth.has_membership('Sales checkout')
             or auth.has_membership('Cashier')
@@ -10,6 +12,20 @@
             )
 def scan_ticket():
     return dict()
+
+
+def validate_sale_form(form):
+    payments = form.vars.payments_data.split(',')
+    total = 0
+    for payment in payments:
+        if not payment:
+            continue
+        payment_id, amount, change, account = payment.split(':')
+        total += DQ(amount)
+    if total >= form.vars.total:
+        print total
+    else:
+        form.errors.payments_data = T('Payments amount is lower than the total')
 
 
 @auth.requires(auth.has_membership('Sales checkout')
@@ -44,27 +60,52 @@ def create():
         total += (bag_item.sale_price + bag_item.sale_taxes) * bag_item.quantity
         quantity += bag_item.quantity
         reward_points += bag_item.id_item.reward_points or 0
+    subtotal = DQ(subtotal, True)
+    taxes = DQ(taxes, True)
+    total = DQ(total, True)
+    quantity = DQ(quantity, True)
+    reward_points = DQ(reward_points, True)
+
     form = SQLFORM(db.sale)
     form.vars.id_bag = bag.id
-    form.vars.subtotal = DQ(subtotal, True)
-    form.vars.taxes = DQ(taxes, True)
-    form.vars.total = DQ(total, True)
-    form.vars.quantity = DQ(quantity, True)
+    form.vars.subtotal = DQ(subtotal)
+    form.vars.taxes = DQ(taxes)
+    form.vars.total = DQ(total)
+    form.vars.quantity = DQ(quantity)
     form.vars.reward_points = reward_points
     form.vars.id_store = bag.id_store.id
 
-    form[0].insert(-1, sqlform_field('payments', 'Payments', None))
+    payment_options_data = {}
 
-    if form.process().accepted:
+    payment_options = db(db.payment_opt.is_active == True).select()
+    payments_toolbar = DIV(_class='btn-toolbar btn-justified', _role="toolbar")
+    payments_select = DIV(_class="btn-group", _role="group", _id="payment_options")
+    for payment_option in payment_options:
+        classes = "btn-primary" if payment_option == payment_options.first() else ""
+        payments_select.append(A(payment_option.name, _class='payment_opt btn btn-default ' + classes, _value=payment_option.id, _id="payment_opt_%s"%payment_option.id))
+
+        payment_options_data[str(payment_option.id)] = {'allow_change': payment_option.allow_change, 'name': payment_option.name}
+    payments_toolbar.append(payments_select)
+    payments_toolbar.append(DIV(A(I(_class="fa fa-plus"), _class="btn btn-default"), _class="btn-group", _role="group", _id="add_payment"))
+    payments_row = DIV( payments_toolbar, DIV(_id="payments_list"), INPUT(_id="payments_data" ,_name="payments_data", _hidden=True), SCRIPT('var payment_options_data = %s; selected_payment_opt = %s' % (json.dumps(payment_options_data), payment_options.first().id)) )
+
+    form[0].insert(-1, sqlform_field('payments', 'Payments', payments_row))
+    form[0].insert(0, sqlform_field('', T('Reward Points'), reward_points))
+    form[0].insert(0, sqlform_field('', T('Quantity'), quantity))
+    form[0].insert(0, sqlform_field('', T('Total'), total))
+    form[0].insert(0, sqlform_field('', T('Taxes'), taxes))
+    form[0].insert(0, sqlform_field('', T('Subtotal'), subtotal))
+
+    if form.process(onvalidation=validate_sale_form).accepted:
         sale = db.sale(form.vars.id)
         # set sale parameters
         sale.consecutive = db.store(session.store).consecutive
-        sale.id_bag = bag.id
-        sale.subtotal = DQ(subtotal)
-        sale.total = DQ(total)
-        sale.quantity = DQ(quantity)
-        sale.reward_points = reward_points
-        sale.id_store = session.store
+        # sale.id_bag = bag.id
+        # sale.subtotal = DQ(subtotal)
+        # sale.total = DQ(total)
+        # sale.quantity = DQ(quantity)
+        # sale.reward_points = reward_points
+        # sale.id_store = session.store
         sale.update_record()
 
         db.sale_log.insert(id_sale=sale.id, sale_event="paid")
@@ -74,7 +115,7 @@ def create():
         response.flash = T('Sale created')
     elif form.errors:
         response.flash = T('form has errors')
-    return dict(form=form)
+    return dict(form=form, subtotal=subtotal, taxes=taxes, total=total, quantity=quantity, bag=bag)
 
 
 @auth.requires(auth.has_membership('Admin')
