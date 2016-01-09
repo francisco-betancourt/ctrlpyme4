8e57b7b3065ac62aaf17ca68437f5b1954fd0b9b
Daniel J. Ramirez <danielramirezz123@gmail.com>
2016-01-09 12:30:44 -0600

    Configuration available without store selection

--- models/HTMLUtils.py
+++ models/HTMLUtils.py
@@ -294,6 +294,15 @@ def super_table(table, fields, query, row_function=default_row_function,
     return table
 
 
+def ticket(title, store, seller, items, barcode, footer):
+    ticket = DIV(
+        DIV(_class="logo"),
+        P(title),
+        _id="ticket", _class="ticket"
+    )
+
+
+
 
 def sqlform_field(id, label, content):
     return DIV(
@@ -301,93 +310,3 @@ def sqlform_field(id, label, content):
                 DIV(content, _class="col-sm-9", _id=id + '_contents' ),
                 _id=id, _class="form-group"
             )
-
-
-
-def data_row(row, fields=[], deletable=True, editable=True, extra_options=[], controller=None, _vars={}, selectable=True):
-    """ """
-    options_enabled = deletable or editable or extra_options
-
-    if not controller:
-        controller = request.controller
-
-    # per row checkbox
-    tr = TH()
-    if selectable:
-        tr.append(INPUT(_type='checkbox', _class='row_checkbox', _value=row.id))
-    for field in fields:
-        td = ''
-        # if the field is a list, we iterate over its elements to concatenate the the row fields into a  single column
-        if type(field) == type([]):
-            for inner_field in field:
-                td += row[inner_field] + ' '
-        else:
-            td = row[field]
-        tr.append(TD(td))
-    # Options
-    if options_enabled:
-        vars_string = URL(vars=_vars).split('?')[1] if _vars else ''
-        options_td = TD()
-        if editable:
-            options_td.append(option_btn('pencil', URL(controller, 'update', args=row.id, vars=_vars)))
-        if deletable:
-            delete_action = 'delete_rows("/%s", "", "%s")' % (row.id, vars_string)
-            options_td.append(option_btn('eye-slash', onclick=delete_action))
-        # options must be related to the row, for that reason this function only allows same controller urls, and also it asumes that the first argument in the specified action is the row id.
-        if extra_options:
-            for option in extra_options:
-                icon_name = option['icon_name'] if option.has_key('icon_name') else ''
-                url_action = option['url_action'] if option.has_key('url_action') else None
-                onclick = option['onclick'] if option.has_key('onclick') else None
-                url_args=option['url_args'] if option.has_key('url_args') else []
-                url_args.insert(0, row.id)
-                url = URL(controller, url_action, args=url_args, vars=_vars)
-
-                option_name = option['name'] if option.has_key('name') else ''
-
-                options_td.append(option_btn(icon_name, url, option_name, onclick))
-        tr.append(options_td)
-
-        return tr
-
-
-def data_headers(headers=[], options_enabled=True, selectable=True):
-    # the master checkbox
-    thead = TH()
-    if selectable:
-        thead.append(INPUT(_type='checkbox', _id='master_checkbox'))
-    for header in headers:
-        thead.append(TH(T(header)))
-    if options_enabled:
-        thead.append(TH(T('Options')))
-
-    return thead
-
-
-def data_table(head_cols, rows):
-    pass
-
-
-def data_table(headers=[], rows=[], fields=[], deletable=True,
-               editable=True, extra_options=[], controller='', _vars={},
-               selectable=True):
-    """ Creates a data table with multiselect via checkboxes
-
-        headers: the table headers
-        rows: the set of rows obtained from db
-        fields: the fields of the row that will be placed as table data, a field can be a string or a list, when the field is a list, all the list fields will be placed as a single column.
-    """
-
-    options_enabled = deletable or editable or extra_options
-
-    # the master checkbox
-    thead = data_headers(headers=headers, options_enabled=options_enabled, selectable=selectable)
-    thead = THEAD(thead)
-    tbody = TBODY()
-    for row in rows:
-        tr = data_row(row, fields=fields, deletable=deletable, editable=editable, extra_options=extra_options, controller=controller, _vars=_vars, selectable=selectable)
-        tbody.append(tr)
-    table = TABLE(thead, tbody, _class="table table-hover")
-    table = DIV(table, _class="table-responsive") # responsiveness
-
-    return table
--- models/settup.py
+++ models/settup.py
@@ -1,16 +1,20 @@
 # create extra Options
 if auth.is_logged_in():
-    if not session.store:
-        stores = db(db.store.is_active == True).select()
-        if len(stores) == 1:
-            session.store = stores.first().id
-    else:
-        if not auth.has_membership('Store %s' % session.store) and not auth.has_membership('Admin'):
-            redirect(URL('user', 'store_selection'))
 
-    # redirect to store selection, when the user is a employee
-    if not session.store and (request.controller != 'user' or request.function != 'store_selection') and auth.has_membership('Employee'):
-        redirect(URL('user', 'store_selection'))
+    if not request.controller in ['address', 'appadmin', 'brand', 'category', 'default', 'item', 'item_image', 'measure_unit', 'payment_opt', 'search', 'store', 'tax', 'trait', 'trait_category', 'user', 'wallet']:
+
+        # select the first store if theres only one
+        if not session.store:
+            stores = db(db.store.is_active == True).select()
+            if len(stores) == 1:
+                session.store = stores.first().id
+        else:
+            if not auth.has_membership('Store %s' % session.store) and not auth.has_membership('Admin'):
+                redirect(URL('user', 'store_selection'))
+
+        # redirect to store selection, when the user is a employee
+        if not session.store and (request.controller != 'user' or request.function != 'store_selection') and auth.has_membership('Employee'):
+            redirect(URL('user', 'store_selection'))
 
 
     # Automatic bag creation
