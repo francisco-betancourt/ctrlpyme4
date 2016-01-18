6719289c1beb38072ff9bc69e0ff7ff8eda49dae
Francisco Betancourt <francisco@betanetweb.com>
2016-01-18 17:27:45 -0600

    Step one for store CSD, certificate reading correctly

--- controllers/store.py
+++ controllers/store.py
@@ -50,6 +50,46 @@ def index():
     )
     return locals()
 
+def read_certificate(form):
+    from cgi import FieldStorage
+    #Check if you received both the certificate and private key
+    print form.vars
+    if form.vars.private_key=='' and form.vars.certificate=='':
+        form.errors.certificate=T("Please input your certificate")
+        form.errors.private_key=T("Please input your private key")
+    if isinstance(form.vars.private_key,FieldStorage) and not isinstance(form.vars.certificate,FieldStorage):
+        form.errors.certificate=T("Please input your certificate")
+        form.errors.private_key=T("You must provide a private key and a certificate simultaneously")
+        return
+    if isinstance(form.vars.certificate,FieldStorage) and not isinstance(form.vars.private_key,FieldStorage):
+        form.errors.certificate=T("You must provide a private key and a certificate simultaneously")
+        form.errors.private_key=T("Please input your private key")
+        return
+    #If one is selected for deleting delete both
+    if form.vars.certificate__delete=='on':
+        form.vars.certificate_base64=None
+        form.vars.no_certificado=None
+        return
+    if form.vars.private_key_delete=='on':
+        form.vars.csdpass=None
+    if isinstance(form.vars.certificate,str):
+        return
+    #Read certificate number
+    cert_file=form.vars.certificate.file.read()
+    form.vars.certificate.file.seek(0)
+    from OpenSSL import crypto
+    try:
+        cert = crypto.load_certificate(crypto.FILETYPE_ASN1,cert_file)
+        cert_pem=crypto.dump_certificate(crypto.FILETYPE_PEM,cert)
+    except:
+        form.errors.certificate=T("Invalid certificate file")
+        return
+    form.vars.certificate_number=hex(cert.get_serial_number())[2:-1].decode('hex')
+    #Save certificate as base64
+    from base64 import b64encode
+    form.vars.certificate_base64=b64encode(cert_file)
+    form.vars.certpem_base64=b64encode(cert_pem)
+
 @auth.requires_membership('Admin')
 def seals():
     store = db.store(request.args(0))
@@ -61,5 +101,8 @@ def seals():
     form[0].insert(2,DIV(
         LABEL(T('CSD password'),_class="control-label col-sm-3",_for="csdpass",_id="store_csdpass_label"),
         DIV(INPUT(_type="password", _name="csdpass",_id="store_csdpass",_class="form-control string"),_class="col-sm-9"),_class="form-group",_id="store_csdpass__row", _style="display:none;"))
-
+    #On validation check for the certificate on accepted process the private key
+    if form.process(onvalidation=read_certificate).accepted:
+        #Now transform and store the user seals
+        print "Ok"
     return locals()
