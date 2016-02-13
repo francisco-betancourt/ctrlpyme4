80c0b91443abf447d6a904699ee698f189fb7436
Daniel J. Ramirez <djrmuv@gmail.com>
2016-02-13 14:59:51 -0600

    Basic discount addition

--- controllers/offer_group.py
+++ controllers/offer_group.py
@@ -13,11 +13,47 @@ def create():
 
 
 @auth.requires_membership('Offers')
-def add_item_discount():
-    """ args: [id_offer_group] """
-    offer_group = db.offer_group(request.args(0))
+def add_discount():
+    """
+        args: [id_offer_group]
+        vars: [target_id, target_name, percentage, code, is_coupon, is_combinable]
+    """
 
-    return dict()
+    offer_group = db.offer_group(request.args(0))
+    if not offer_group:
+        raise HTTP(404)
+    print request.vars
+
+    error = None
+    msg = T('Discount added')
+    target_name = request.vars.target_name
+    if not target_name in ['id_item', 'id_brand', 'id_category']:
+        error = T('Invalid target')
+    try:
+        target_id = int(request.vars.target_id)
+    except:
+        error = T('Invalid target')
+    percentage = request.vars.percentage
+    code = request.vars.code
+    is_coupon = request.vars.is_coupon == 'true'
+    is_combinable = request.vars.is_combinable == 'true'
+
+    if not error:
+        discount_data = {
+            'id_offer_group':  offer_group.id
+            , target_name: target_id
+            , 'percentage': percentage
+            , 'code': code
+            , 'is_coupon': is_coupon
+            , 'is_combinable': is_combinable
+        }
+        ret = db.discount.validate_and_insert(**discount_data)
+        if ret.errors:
+            error = T('Something went wrong')
+
+    msg = error if error else msg
+    session.info = msg
+    redirect(URL('fill', args=offer_group.id));
 
 
 @auth.requires_membership('Offers')
