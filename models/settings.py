40936a41aa2cc07d39047c491062d052a660cbc2
Daniel J. Ramirez <djrmuv@gmail.com>
2016-06-18 13:11:09 -0500

    Added missing license header in some files

--- models/settings.py
+++ models/settings.py
@@ -1,4 +1,24 @@
-# settings list
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2016 Bet@net
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+# Author Daniel J. Ramirez <djrmuv@gmail.com>
+
+# This module is used to query the application settings and provide useful constants, to all the application.
 
 from datetime import timedelta
 from constants import FLOW_BASIC
