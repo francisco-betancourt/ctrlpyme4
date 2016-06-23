73146dcab607a06dda11271f1b1c1a1578e398ae
Daniel J. Ramirez <djrmuv@gmail.com>
2016-06-23 13:47:21 -0500

    Collect garbage every request, this seems to solve the stalled memory problem

--- models/settup.py
+++ models/settup.py
@@ -4,6 +4,10 @@ from bag_utils import auto_bag_selection
 from item_utils import item_barcode
 from constants import *
 
+# avoid excesive memory usage, this is a workaround since web2py or the application seems to be consuming a lot of memory, TODO: further investigation of the problem.
+import gc
+gc.collect()
+
 enable_bootstrap=True
 enable_treeview=True
 enable_css_item_card=True
