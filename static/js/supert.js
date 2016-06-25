fd054208936f4861b77c3a676eaee05d5288e5cd
Daniel J. Ramirez <djrmuv@gmail.com>
2016-06-25 14:28:06 -0500

    Fixed supert problem when no pages available

--- static/js/supert.js
+++ static/js/supert.js
@@ -80,76 +80,80 @@
 
     var ipp_value = supert.querySelector('.ipp-value');
     var ipp_form = supert.querySelector('.st-ipp-form');
+    if (ipp_value && ipp_form) {
+      ipp_value.addEventListener('click', function(event) {
+        this.setAttribute('hidden', 'hidden');
+        ipp_form.removeAttribute('hidden');
+        var input = ipp_form.querySelector('.form-control');
+        input.focus();
+        input.setSelectionRange(0, input.value.length);
+      });
+      ipp_form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        var new_ipp = ipp_form.querySelector('.form-control').value;
+        URL.vars['ipp_' + t_index] = new_ipp;
+        URL.vars['page_' + t_index] = "";
+        window.location.href = URL.generate_url();
+      });
+      ipp_form.addEventListener('focusout', function (event) {
+        ipp_value.removeAttribute('hidden');
+        this.setAttribute('hidden', 'hidden');
+      });      
+    }
 
-    ipp_value.addEventListener('click', function(event) {
-      this.setAttribute('hidden', 'hidden');
-      ipp_form.removeAttribute('hidden');
-      var input = ipp_form.querySelector('.form-control');
-      input.focus();
-      input.setSelectionRange(0, input.value.length);
-    });
-    ipp_form.addEventListener('submit', function(event) {
-      event.preventDefault();
-      var new_ipp = ipp_form.querySelector('.form-control').value;
-      URL.vars['ipp_' + t_index] = new_ipp;
-      URL.vars['page_' + t_index] = "";
-      window.location.href = URL.generate_url();
-    });
-    ipp_form.addEventListener('focusout', function (event) {
-      ipp_value.removeAttribute('hidden');
-      this.setAttribute('hidden', 'hidden');
-    });
 
     var page_value = supert.querySelector('.page-value');
     var page_form = supert.querySelector('.st-page-form');
-
-    page_value.addEventListener('click', function(event) {
-      this.setAttribute('hidden', 'hidden');
-      page_form.removeAttribute('hidden');
-      var select = page_form.querySelector('.form-control');
-      select.focus();
-      select.click();
-    });
-    page_form.addEventListener('change', function(event) {
-      event.preventDefault();
-      var new_page = page_form.querySelector('.form-control').value;
-      URL.vars['page_' + t_index] = new_page;
-      window.location.href = URL.generate_url();
-    });
-    page_form.addEventListener('focusout', function (event) {
-      page_value.removeAttribute('hidden');
-      this.setAttribute('hidden', 'hidden');
-    });
-
-
-    function getCheckedCbs() {
-      var cbs = supert.querySelectorAll('input[type="checkbox"]:checked');
-      return cbs;
-    }
-    function countChecked() {
-      var n = getCheckedCbs().length;
-      supert.querySelector('#supert_card_header');
-    };
-    var cb_master = document.getElementById('cb_master');
-    if (cb_master) {
-      var cbs = supert.querySelectorAll('input[type="checkbox"]:not(#cb_master)');
-      for (var index = 0; index < cbs.length; index++) {
-        var the_cb = cbs[index];
-        the_cb.addEventListener('change', function (event) {
-          countChecked();
-        })
-      }
-      cb_master.addEventListener('change', function (event) {
-        for (var index = 0; index < cbs.length; index++) {
-          if (event.target.checked) {
-            cbs[index]._change(true);
-          } else {
-            cbs[index]._change(false);
-          }
-        }
-        countChecked();
+    if (page_value && page_form) {
+      page_value.addEventListener('click', function(event) {
+        this.setAttribute('hidden', 'hidden');
+        page_form.removeAttribute('hidden');
+        var select = page_form.querySelector('.form-control');
+        select.focus();
+        select.click();
+      });
+      page_form.addEventListener('change', function(event) {
+        event.preventDefault();
+        var new_page = page_form.querySelector('.form-control').value;
+        URL.vars['page_' + t_index] = new_page;
+        window.location.href = URL.generate_url();
+      });
+      page_form.addEventListener('focusout', function (event) {
+        page_value.removeAttribute('hidden');
+        this.setAttribute('hidden', 'hidden');
       });
     }
+
+
+    // will be USED FOR FUTURE MULTISELECT OPTION
+  //   function getCheckedCbs() {
+  //     var cbs = supert.querySelectorAll('input[type="checkbox"]:checked');
+  //     return cbs;
+  //   }
+  //   function countChecked() {
+  //     var n = getCheckedCbs().length;
+  //     supert.querySelector('#supert_card_header');
+  //   };
+  //   var cb_master = document.getElementById('cb_master');
+  //   if (cb_master) {
+  //     var cbs = supert.querySelectorAll('input[type="checkbox"]:not(#cb_master)');
+  //     for (var index = 0; index < cbs.length; index++) {
+  //       var the_cb = cbs[index];
+  //       the_cb.addEventListener('change', function (event) {
+  //         countChecked();
+  //       })
+  //     }
+  //     cb_master.addEventListener('change', function (event) {
+  //       for (var index = 0; index < cbs.length; index++) {
+  //         if (event.target.checked) {
+  //           cbs[index]._change(true);
+  //         } else {
+  //           cbs[index]._change(false);
+  //         }
+  //       }
+  //       countChecked();
+  //     });
+  //   }
   }
 
   var superts = document.querySelectorAll('.supert');
